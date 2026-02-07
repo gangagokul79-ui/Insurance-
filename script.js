fbbb6c0cac17b2be3132bf18a099d733a82fed23
Gangadevi <gangagokul79@gmail.com>
2026-02-07 14:19:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-function showText(id) {
-    const allTexts = document.querySelectorAll('.solution-text');
-
-    allTexts.forEach(text => {
-        if (text.id === id) {
-            text.style.display =
-                text.style.display === "block" ? "none" : "block";
-        } else {
-            text.style.display = "none";
-        }
-    });
-}
-
-function applyNow() {
-    document.getElementById("applyMsg").style.display = "block";
-}
+console.log("Script connected");
